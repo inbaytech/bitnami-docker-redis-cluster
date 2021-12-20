ba339a2af18f65317f48dd8fdd157d0129f22d2e
John Marshall <john.marshall@inbaytech.com>
2021-12-20 10:40:36 -0500

    Add 6.2 for RHEL 8

---
+++ 6.2/rhel-8/rootfs/opt/bitnami/scripts/redis-cluster/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -o xtrace # Uncomment this line for debugging purpose
+
+echo "Initial environment dump starts"
+env
+echo "Initial environment dump ends"
+
+# Load Redis environment variables
+. /opt/bitnami/scripts/redis-cluster-env.sh
+
+# Load libraries
+. /opt/bitnami/scripts/libbitnami.sh
+. /opt/bitnami/scripts/librediscluster.sh
+
+print_welcome_page
+
+if [[ "$*" = *"/run.sh"* ]]; then
+    info "** Starting Redis setup **"
+    /opt/bitnami/scripts/redis-cluster/setup.sh
+    info "** Redis setup finished! **"
+fi
+
+echo ""
+exec "$@"
